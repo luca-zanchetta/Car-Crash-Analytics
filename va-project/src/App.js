9c9aad7e25e39aa88753cc6cfed80e0b9c9d98ef
Mattia Aquilina <mattia.eagle@gmail.com>
2024-01-24 12:08:42 +0100

    Init react app

---
+++ va-project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import * as d3 from "d3"
+import LinePlot from './Charts/test';
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+
+
+export default App;
