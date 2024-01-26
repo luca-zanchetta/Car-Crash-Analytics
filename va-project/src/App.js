b49f8b757205104e94a7a650ed3766e55e790f2d
Mattia Aquilina <mattia.eagle@gmail.com>
2024-01-26 11:01:30 +0100

    Interface init

--- va-project/src/App.js
+++ va-project/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import * as d3 from "d3"
 import LinePlot from './Charts/test';
@@ -7,6 +6,28 @@ import LinePlot from './Charts/test';
 function App() {
   return (
     <div className="App">
+      <div className='TopBar'>
+        <h1>Car crash anlytics</h1>
+      </div>
+      <div className='ScreenCenter'>
+        <div className='Filters'>
+
+        </div>
+        <div className='Map'>
+
+        </div>
+      </div>
+      <div className='ScreenBottom'>
+        <div className='DimReduction'>
+          <LinePlot data={[12, 5, 6, 6, 9, 10]} ></LinePlot>
+        </div>
+        <div className='ParallelCoordinates'>
+
+        </div>
+        <div className='Heatmap'>
+
+        </div>
+      </div>
     </div>
   );
 }
--- va-project/src/Charts/test.js
+++ va-project/src/Charts/test.js
@@ -1,1 +1,28 @@
-import * as d3 from "d3"
+import * as d3 from "d3";
+import "./Charts.css"
+import { useDimensions } from "./useDimensions.ts";
+import { useRef } from "react";
+
+
+export default function LinePlot({
+  data,  
+  margin = 10,
+}) {
+  const chartRef = useRef(null);
+  const chartSize = useDimensions(chartRef);
+
+  const x = d3.scaleLinear([0, data.length - 1], [margin, chartSize.width - margin]);
+  const y = d3.scaleLinear(d3.extent(data), [chartSize.height - margin, margin]);
+  const line = d3.line((d, i) => x(i), y);
+
+  return (
+    <div className="Chart" ref={chartRef}>
+        <svg width={chartSize.width} height={chartSize.height}>
+        <path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
+        <g fill="white" stroke="currentColor" stroke-width="1.5">
+            {data.map((d, i) => (<circle key={i} cx={x(i)} cy={y(d)} r="2.5" />))}
+        </g>
+        </svg>
+    </div>
+  );
+}
