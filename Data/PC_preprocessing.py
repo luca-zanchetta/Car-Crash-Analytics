ab3e77985e27cb9adaf644ca7a07605f839711e2
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2024-01-30 10:25:59 +0100

    Parallel coordinate data preprocessing

--- Data/PC_preprocessing.py
+++ Data/PC_preprocessing.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 def encode(orignial_set):
     encoding_dict = {value:code for code, value in enumerate(orignial_set)}
-    encoded_set = {encoding_dict[value] for value in orignial_set}
-    return encoded_set
+    return encoding_dict
 
 """
 Attributes (initial):
@@ -17,28 +16,67 @@ Weather_Conditions
 Vehicle_Type
 """
 
-data = pd.read_csv('./Data/dataset.csv')
+data = pd.read_csv('./va-project/public/dataset.csv')
+ids = []
+
+for index, row in data.iterrows():
+    ids.append(index)
+
+data['Id'] = ids
 
 junction_control = set()
 junction_detail = set()
 light_conditions = set()
 road_surface_conditions = set()
 road_type = set()
-urban_rural = set()
 weather_conditions = set()
 vehicle_type = set()
+
+vehicles = []
+
+for index, row in data.iterrows():
+    vehicle = row['Vehicle_Type']
+    
+    if vehicle == 'Car':
+        vehicles.append('Car')
+    elif vehicle == 'Motorcycle over 500cc':
+        vehicles.append('Motorcycle')
+    elif vehicle == 'Motorcycle 50cc and under':
+        vehicles.append('Motorcycle')
+    elif vehicle == 'Minibus (8 - 16 passenger seats)':
+        vehicles.append('Minibus')
+    elif vehicle == 'Motorcycle 125cc and under':
+        vehicles.append('Motorcycle')
+    elif vehicle == 'Goods over 3.5t. and under 7.5t':
+        vehicles.append('Good')
+    elif vehicle == 'Taxi/Private hire car':
+        vehicles.append('Car')
+    elif vehicle == 'Bus or coach (17 or more pass seats)':
+        vehicles.append('Bus')
+    elif vehicle == 'Motorcycle over 125cc and up to 500cc':
+        vehicles.append('Motorcycle')
+    elif vehicle == 'Agricultural vehicle':
+        vehicles.append('Agricultural vehicle')
+    elif vehicle == 'Goods 7.5 tonnes mgw and over':
+        vehicles.append('Good')
+    elif vehicle == 'Van / Goods 3.5 tonnes mgw or under':
+        vehicles.append('Good')
+    else:
+        vehicles.append('Other vehicle')
+
+data['Vehicle_Type'] = vehicles
 
 # Create sets of unique values
 for index, row in data.iterrows():
     junction_control.add(row['Junction_Control'])                   # 5 ok
-    junction_detail.add(row['Junction_Detail'])                     # 9 ok?
+    junction_detail.add(row['Junction_Detail'])                     # 9 ok
     light_conditions.add(row['Light_Conditions'])                   # 5 ok
     road_surface_conditions.add(row['Road_Surface_Conditions'])     # 4 ok
     road_type.add(row['Road_Type'])                                 # 5 ok
-    urban_rural.add(row['Urban_or_Rural_Area'])                     # 1 (forse abbiamo preso righe sbagliate)
     weather_conditions.add(row['Weather_Conditions'])               # 7 ok
-    vehicle_type.add(row['Vehicle_Type'])                           # 13
 
+for v in vehicles:
+    vehicle_type.add(v)
 
 # Encode the sets
 junction_control_enc = encode(junction_control)
@@ -46,6 +84,16 @@ junction_detail_enc = encode(junction_detail)
 light_conditions_enc = encode(light_conditions)
 road_surface_conditions_enc = encode(road_surface_conditions)
 road_type_enc = encode(road_type)
-urban_rural_enc = encode(urban_rural)
 weather_conditions_enc = encode(weather_conditions)
-vehicle_type = encode(vehicle_type)
+vehicle_type_enc = encode(vehicle_type)
+
+for index, row in data.iterrows():
+    row['Junction_Control'] = junction_control_enc[row['Junction_Control']]
+    row['Junction_Detail'] = junction_detail_enc[row['Junction_Detail']]
+    row['Light_Conditions'] = light_conditions_enc[row['Light_Conditions']]
+    row['Road_Surface_Conditions'] = road_surface_conditions_enc[row['Road_Surface_Conditions']]
+    row['Road_Type'] = road_type_enc[row['Road_Type']]
+    row['Weather_Conditions'] = weather_conditions_enc[row['Weather_Conditions']]
+    row['Vehicle_Type'] = vehicle_type_enc[row['Vehicle_Type']]
+
+data.to_csv('./va-project/public/dataset.csv', index=False, sep=',')
